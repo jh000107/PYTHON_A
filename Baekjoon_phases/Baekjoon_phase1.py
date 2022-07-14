fcc6ad17ca7df8407dfa5ecb02588806aa00fcac
jh000107 <junhuicho8@gmail.com>
2022-07-14 20:10:11 +0900

    Few questions done for phase 1

---
+++ Baekjoon_phases/Baekjoon_phase1.py
@@ -0,0 +1,22 @@
+"""
+    #2557
+    Print Hello World!
+"""
+print('Hello World!')
+
+"""
+    #10718
+    Print "강한친구 대한육군" twice. Once in each row
+"""
+
+print('강한친구 대한육군')
+print('강한친구 대한육군')
+
+"""
+    #1000
+    Take two inputs, and print the sum
+"""
+
+a, b= map(int, input().split())
+print(a+b)
+
