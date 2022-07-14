be852f52614d0f28f0d2c891344f90514a758be3
jh000107 <junhuicho8@gmail.com>
2022-07-15 01:07:52 +0900

    Added a README file, and added more questions

--- Baekjoon_phases/Baekjoon_phase1.py
+++ Baekjoon_phases/Baekjoon_phase1.py
@@ -20,3 +20,35 @@ print('강한친구 대한육군')
 a, b= map(int, input().split())
 print(a+b)
 
+
+"""
+    #10926
+    Put ??! after whatever the input is
+"""
+
+name = input()
+print(name + "??!")
+
+"""
+    #18108
+    서기 연도 vs 불기 연도
+"""
+
+bulgi = int(input())
+print(bulgi-543)
+
+
+"""
+    #10430
+    Take 3 inputs: A,B,C
+    The question is asking to see if
+    (A+B)%C =? ((A%C)+(B%C))%C
+    (AXB)%C =? ((A%C)X(B%C))%C
+"""
+
+a,b,c = map(int,input().split())
+first = (a+b)%c
+second = ((a%c)+(b%c))%c
+third = (a*b)%c
+fourth = ((a%c)*(b%c))%c
+print(first, '\n',second, '\n', third, '\n', fourth, sep='')
---
+++ Baekjoon_phases/README.md
@@ -0,0 +1,16 @@
+# 'Step by Step'
+
+These are python practice problems from the coding website called BAEKJOON ([Baekjoon Online Judge (acmicpc.net)](https://www.acmicpc.net/)). All of these are my solutions to the problems that are posted on the website.
+
+> Phases:
+>
+> 			1. Input, Output, and Arithmetic operations
+> 			1. IF statement
+> 			1. FOR, WHILE statements
+> 			1. Array, List
+> 			1. Functions
+> 			1. Character string
+> 			1. Fundamental Mathematics I
+> 			1. Fundamental Mathematics II
+> 			1. Recursive Function
+> 			1. Brute Force Algorithm
