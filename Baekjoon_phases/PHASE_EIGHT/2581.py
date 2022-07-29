4bb6bae2c41e301a9948649068efa5bdbe626fc4
jh000107 <junhuicho8@gmail.com>
2022-07-29 23:59:40 +0900

    Another question added

---
+++ Baekjoon_phases/PHASE_EIGHT/2581.py
@@ -0,0 +1,22 @@
+"""
+    #2581
+    More prime numbers. Given two integers M, N, find the sum of all prime numbers between them, including M and N. Also, find the min prime number
+"""
+M = int(input())
+N = int(input())
+lst = []
+# Brought helper function from prev question
+def prime_num(num):
+    if num == 1:
+        return False
+    for i in range(2, num):
+        if num%i == 0:
+            return False
+    return True
+
+for i in range(M, N+1):
+    if prime_num(i):
+        lst.append(i)
+
+print(sum(lst))
+print(min(lst))
