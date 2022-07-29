e515a62178b0840a85be84a3d9b957a01af056e7
jh000107 <junhuicho8@gmail.com>
2022-07-29 23:41:39 +0900

    PHASE_EIGHT question added

--- Baekjoon_phases/PHASE_EIGHT/1978.py
+++ Baekjoon_phases/PHASE_EIGHT/1978.py
@@ -1,4 +1,19 @@
 """
     #1978
-    Finding a Prime number
+    Given N integers, find the number of prime numbers.
 """
+N = int(input())
+lst = map(int,input().split())
+
+def prime_num(num):
+    for i in range(2, num):
+        if num%i == 0:
+            return False
+    return True
+
+ans = 0
+for i in lst:
+    if prime_num(i):
+        ans += 1
+
+print(ans)
