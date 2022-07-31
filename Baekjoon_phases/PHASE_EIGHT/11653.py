c6489cde5a0789b63ee0bcc85618cedc268c77f6
jh000107 <junhuicho8@gmail.com>
2022-07-31 23:11:16 +0900

    Run Time Error

--- Baekjoon_phases/PHASE_EIGHT/11653.py
+++ Baekjoon_phases/PHASE_EIGHT/11653.py
@@ -5,7 +5,22 @@
 
 N = int(input())
 # helper function
-def find_min_prime(num):
 
-# while(N > 1):
+def prime_num(num):
+    if num == 1:
+        return False
+    for i in range(2, num):
+        if num%i == 0:
+            return False
+    return True
 
+
+while(N >= 1):
+    if N == 1:
+        break
+    for i in range(2,N+1):
+        if prime_num(i) and N%i==0:
+            N = int(N/i)
+            print(i)
+            break
+    
