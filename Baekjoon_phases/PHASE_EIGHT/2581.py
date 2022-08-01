6a6510a4d2bc0a562620e41271935ce7cd26c9cf
jh000107 <junhuicho8@gmail.com>
2022-08-01 23:16:10 +0900

    Some modifications

--- Baekjoon_phases/PHASE_EIGHT/2581.py
+++ Baekjoon_phases/PHASE_EIGHT/2581.py
@@ -18,5 +18,8 @@ for i in range(M, N+1):
     if prime_num(i):
         lst.append(i)
 
-print(sum(lst))
-print(min(lst))
+if len(lst) == 0:
+    print(-1)
+else:
+    print(sum(lst))
+    print(min(lst))
